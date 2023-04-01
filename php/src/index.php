fa29b7aa98c56c6a6a960ab3b0b3ae4bdd16d65f
zancrash <farzanali1296@gmail.com>
2023-04-01 19:17:36 -0400

    read from NameFile to create and populate Name Table

--- php/src/index.php
+++ php/src/index.php
@@ -14,3 +14,43 @@ if ($conn->connect_error) {
 } else {
     print("Connected to MySQL server successfully!\n");
 }
+
+
+$sql = "SELECT StudentID from Name_Table";
+$result = mysqli_query($conn, $sql);
+
+if (empty($result)) {
+    $sql = "CREATE TABLE Name_Table (
+        StudentID int,
+        StudentName varchar(255)
+    )";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+}
+
+// Check Name Table for records:
+$sql = "SELECT * FROM Name_Table";
+// Execute query:
+$result = mysqli_query($conn, $sql);
+
+// If Name Table does not contain records:
+if (mysqli_num_rows($result) < 25) {
+    $namefile = fopen('NameFile.txt','r');
+
+    while (!feof($namefile)) {
+        $getTextLine = fgets($namefile);
+        $explodeLine = explode(", ",$getTextLine);
+        
+        list($studentid, $name) = $explodeLine;
+        
+        $query = "INSERT INTO Name_Table (StudentID, StudentName) values('".$studentid."','".$name."')";
+        mysqli_query($conn,$query);
+    }
+    
+    fclose($namefile);
+}
