2e36eaaeb21ba2cbd43b2b4546f31208055f1c86
adamantium7 <lucky.ayuste@gmail.com>
2023-04-02 18:57:57 -0400

    added redirect to main.html

--- php/src/index.php
+++ php/src/index.php
@@ -1,5 +1,5 @@
 <?php
-
+ob_start();
 include __DIR__ . '/Helper/DotEnv.php';
 (new DotEnv(__DIR__ . '/.env'))->load();
 
@@ -7,6 +7,7 @@ $host = 'db';
 $user = 'root';
 $pass = getenv('MYSQL_ROOT_PASSWORD');
 
+ob_flush();
 // check the MySQL connection status
 $conn = new mysqli($host, $user, $pass, 'cp476');
 if ($conn->connect_error) {
@@ -113,10 +114,17 @@ $stmt->bind_param("i", $id);
 $id = $_POST['id'];
 $stmt->execute();
 $result = $stmt->get_result();
-$student_name = mysqli_fetch_row($result)[1]; #obtain student name from result
+$student_name = mysqli_fetch_array($result)[1]; #obtain student name from result
+
+#set post variable
+$_POST['studentname'] = $student_name;
 
 #if student ID is found from name table then get rows from course table containing student id
 if(mysqli_num_rows($result) > 0){
+    
+    #redirect to main.html
+    header("Location: /main.html");
+    exit;
     $stmt = $conn->prepare("SELECT CourseCode, Test1, Test2, Test3, Final FROM Course_Table WHERE StudentID = ?");
     $stmt->bind_param("i", $id);
     $stmt->execute();
@@ -127,6 +135,7 @@ if(mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_array($result)){
         array_push($rows, $row);
     }
+    $_POST['results'] = $rows; #store rows in post variable
 }
 #else student ID does not exist in name table
 else{
