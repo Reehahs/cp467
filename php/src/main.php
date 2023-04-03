42f5c6c6e611abe7da06eacf6f5ddedf57bb7082
adamantium7 <lucky.ayuste@gmail.com>
2023-04-02 20:09:25 -0400

    Create main.php

---
+++ php/src/main.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 50%;
+}
+
+table {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+table tr:nth-child(even){background-color: #f2f2f2;}
+
+table tr:hover {background-color: #ddd;}
+
+table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+    
+table, th, td {
+  border:1px solid black;
+}
+tfoot {
+        font-weight: bold;
+      }
+      .logout-button {
+        float: right;
+        margin-top: 20px;
+        margin-right: 20px;
+        padding: 10px;
+        background-color: #f44336;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      .logout-button:hover {
+        background-color: #d32f2f;
+      }
+</style>
+<body>
+<button class="logout-button">Logout</button>    
+<center>
+<h2>Student Home</h2>
+</center>
+
+<form>
+
+    <div class="container">
+        <label>Student Id : </label>
+        <input type="text" value= <?php echo $_SESSION["id"]; ?> readonly >
+        <label>Name : </label>
+        <input type="text" value=<?php echo $_SESSION["studentname"]; ?> readonly>
+       
+    </div>
+</form>
+<table style="width:100%">
+  <tr>
+    <th>Subject</th>
+    <th>Grades</th>
+  <tbody> 
+  </tr>
+  <tr>
+    <td>MA103</td>
+    <td>90</td>
+   
+  </tr>
+  <tr>
+    <td>CP104</td>
+    <td>85</td>
+    
+  </tr>
+  <tr>
+    <td>CP164</td>
+    <td>70</td>
+    
+  </tr>
+  <tr>
+    <td>Cp212</td>
+    <td>86</td>
+    
+  </tr>
+  <tr>
+    <td>CP312</td>
+    <td>75</td>
+    
+  </tr>
+  <tr>
+    <td>CP414</td>
+    <td>72</td>
+    
+  </tr>
+  <tr>
+    <td>MA102</td>
+    <td>95</td>
+    
+  </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td>Total</td>
+      <td>
+        <!-- JavaScript code to calculate the total grade -->
+        <script>
+          var grades = document.querySelectorAll("tbody td:last-child");
+              var total = 0;
+              for (var i = 0; i < grades.length; i++) {
+                total += parseFloat(grades[i].textContent);
+              }
+              var average = total / grades.length;
+              document.write(average.toFixed(2) + "%");
+        </script>
+      </td>
+    </tr>
+  </tfoot>
+</table>
+
+
+
+</body>
+</html>
+
